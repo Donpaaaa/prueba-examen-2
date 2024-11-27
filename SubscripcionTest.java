c6873cd5eb0bf9cde7566cf5d19a4e4c59aab22c
Donpaaaa <ruizristo@gmail.com>
2024-11-27 16:52:44 +0100

    nuevoss

--- SubscripcionTest.java
+++ SubscripcionTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 class SubscripcionTest {
     Subscripcion subscripcion= new Subscripcion(1200, 12);
-    @test
+    @Test
     void testprecioPorMes() {
         int resultado=100;
         assertEquals(resultado;subscripcion.precioPorMes());
