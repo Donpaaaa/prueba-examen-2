077f00d4be2f46d1c77a8a6b6d41b4ad1f1a4cd1
Donpaaaa <ruizristo@gmail.com>
2024-12-11 14:24:14 +0100

    modificado

--- SubscripcionTest.java
+++ SubscripcionTest.java
@@ -7,5 +7,11 @@ class SubscripcionTest {
         int resultado=100;
         assertEquals(resultado;subscripcion.precioPorMes());
     }
-  
+
+    @Test
+    void testprecioPormes(){
+        this.pruebaprecio= new subscipcion(10,5);
+        int precioesperado1=2;
+        Assertions.assertEquals((double)precioesperado1, this.)
+    }
 }
